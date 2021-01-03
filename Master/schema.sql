d20bdc18fce38d9d771b5f0b003ff9fa45f5333e
vpickard707 <71297613+vpickard707@users.noreply.github.com>
2021-01-02 22:38:44 -0800

    added functions

--- Master/schema.sql
+++ Master/schema.sql
@@ -3,16 +3,24 @@ CREATE database employeeDB;
 
 USE employeeDB;
 
-CREATE TABLE employee_table (
+ CREATE TABLE department (
+
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(30),
+ );
+
+ CREATE TABLE role ( 
+ id INT PRIMARY KEY AUTO_INCREMENT,
+ title VARCHAR(30),
+ salary DECIMAL,
+ department_id INT
+ );
+
+CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
   manager_id INT,
-  raw_total DECIMAL(10,4) NULL,
-  raw_usa DECIMAL(10,4) NULL,
-  raw_uk DECIMAL(10,4) NULL,
-  raw_eur DECIMAL(10,4) NULL,
-  raw_row DECIMAL(10,4) NULL,
   PRIMARY KEY (id)
 );
